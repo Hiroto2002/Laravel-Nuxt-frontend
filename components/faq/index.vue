5a7428912037548936caf54faa1b1f12050758cb
Hiroto2002 <hiroto2002h14@icloud.com>
2023-07-24 15:21:29 +0900

    feat:Accordion

---
+++ components/faq/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Accordion />
+  </div>
+</template>
+<script lang="ts">
+import Accordion from "./Accordion.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    Accordion,
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
     <LayoutsHeader/>
+    <Faq/>
     <LayoutsFooter/>
 </template>
